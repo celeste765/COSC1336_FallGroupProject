7b807bf969897028d1121e3a3aa41eec67f331f0
YvesLangstonMays <yveslangstonmays@gmail.com>
2023-11-23 01:21:08 -0600

    Updated Encryption

--- main.py
+++ main.py
@@ -1,24 +1,17 @@
 import matplotlib.pyplot as plt
 
-fileName = input("Enter the name of the file including the extension.\n")
-print(f"Opening {fileName}.\n")
+
+def addToOrd(value, encryption_key):
+    output = ord(value) + encryption_key
+    return output
 
 
-def readFile(file):
-    # A dictionary to store the characters and their counts
-    fileTestDict = {}
-    lines = []
-    with open(file, "r") as file:
-        for line in file:
-            for character in line:
-                if character in fileTestDict:
-                    fileTestDict[character] += 1
-                else:
-                    fileTestDict[character] = 1
-
-        print(fileTestDict)
-
-    return lines
+def readFile():
+    fileName = input("Enter the name of the file including the extension.\n")
+    print(f"Opening {fileName}.\n")
+    encryption_key = input("Enter an encryption key.\n")
+    print("Encryption key entered.")
+    return (fileName, encryption_key)
 
 
 # This is a dict used to test the plotdata function
@@ -41,8 +34,28 @@ def plotData(data):
 # This function should encrypt some text given a key as an argument
 # The key will be something that makes the encryption unique for that
 # file or text
-def encrypt():
-    pass
+
+
+def encrypt(info):
+    # A dictionary to store the characters and their counts
+    fileTestDict = {}
+    file = info[0]
+    encryption_key = int(info[1])
+    encrypted = ""
+    with open(file, "r") as file:
+        for line in file:
+            stripped_line = line.strip("\n")
+            for character in stripped_line:
+                if character in fileTestDict:
+                    fileTestDict[character] += 1
+                else:
+                    fileTestDict[character] = 1
+
+        # print(fileTestDict)
+    for key in fileTestDict:
+        encrypted += str(addToOrd(key, encryption_key))
+    print(encrypted)
+    return encrypted
 
 
 # This function will do the opposite of the encrypt function given
@@ -53,7 +66,7 @@ def decrypt():
 
 def main():
     print("Add functions below this print statement.\n")
-    readFile(fileName)
+    encrypt(readFile())
 
 
 main()
