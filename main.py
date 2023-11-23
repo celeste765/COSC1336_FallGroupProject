48e745a8018d8d8712dcc6bce61e29f6d2a536a2
YvesLangstonMays <yveslangstonmays@gmail.com>
2023-11-22 22:49:29 -0600

    Updated Plot Function

--- main.py
+++ main.py
@@ -5,12 +5,22 @@ def readFile():
     pass
 
 
-def plotData():
+# This is a dict used to test the plotdata function
+# It can be deleted once the function works
+test_dictionary = {"e": 1, "c": 3, "d": 5}
+
+
+def plotData(data):
     x_vals = []
     y_vals = []
 
-    plt.subplot(x_vals, y_vals)
-    pass
+    for key in data:
+        x_vals.append(key)
+    for key in data:
+        y_vals.append(data[key])
+
+    plt.bar(x_vals, y_vals)
+    plt.show()
 
 
 def encrypt():
@@ -23,6 +33,7 @@ def decrypt():
 
 def main():
     print("Add functions below this print statement.")
+    plotData(test_dictionary)
 
 
 main()
